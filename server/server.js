60f173ca309e43f22d3744ae9449800f0bc14b8c
Gunan jain <168672473+Gunan-Jain@users.noreply.github.com>
2025-04-06 23:30:39 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -129,4 +129,6 @@ app.post("/api/check-voter", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
