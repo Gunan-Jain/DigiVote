213a0123fb1697a020725dfa78f1f0bb772a844e
Gunan jain <168672473+Gunan-Jain@users.noreply.github.com>
2025-04-06 23:29:02 +0530

    Update login.js

--- server/login.js
+++ server/login.js
@@ -2,11 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import EmployeeModel from "./models/employee.js"; // Add .js extension for ES module
+const PORT = process.env.PORT || 3000;
+
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 mongoose.connect(
   "mongodb+srv://gunanjain809:v413ySsA1Iw9kw7h@cluster0.wfmhx.mongodb.net/"
 );
